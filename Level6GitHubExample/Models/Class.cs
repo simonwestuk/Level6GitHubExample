13ca5797ec9814e099a7e007b38661e829bc673e
Simon West <wests@btc.ac.uk>
2021-03-23 15:41:32 +0000

    Added Class

---
+++ Level6GitHubExample/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Level6GitHubExample.Models
+{
+    public class Class
+    {
+    }
+}
